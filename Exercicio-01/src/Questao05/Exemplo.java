020296115db5244cc8ed13339bc431dd8d1f7d43
Fulano de Tal <pedrov6@outlook.com>
2020-12-05 14:07:55 -0300

    Exercicio 01

---
+++ Exercicio-01/src/Questao05/Exemplo.java
@@ -0,0 +1,10 @@
+package Questao05;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 1;
+        //System.out.println("Soma: "+ a+b);
+        System.out.println("Soma: "+ (a+b));
+    }
+}
